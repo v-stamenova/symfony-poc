6fb2d3d4ff7e951329685e7255c6835cbeb7d390
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-02-04 14:46:09 +0100

    Old school validation without attributes

--- src/Model/Task.php
+++ src/Model/Task.php
@@ -6,12 +6,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Task
 {
     public ?int $id;
-
-    #[Assert\NotBlank(message: "Title is required")]
-    #[Assert\Length(min: 3, max: 255, minMessage: "Title must be at least 3 characters long", maxMessage: "Title cannot be longer than 255 characters")]
     public string $title;
-
-    #[Assert\Length(max: 1000, maxMessage: "Description cannot exceed 1000 characters")]
     public ?string $description;
 
     public function __construct(?int $id, string $title, ?string $description)
@@ -19,5 +14,21 @@ class Task
         $this->id = $id;
         $this->title = $title;
         $this->description = $description;
+    }
+
+    public static function validation(string $title, string $description): array {
+        $errors = [];
+
+        if (empty($title)) {
+            $errors[] = "Title is required.";
+        } elseif (strlen($title) < 3 || strlen($title) > 255) {
+            $errors[] = "Title must be between 3 and 255 characters.";
+        }
+
+        if (!empty($description) && strlen($description) > 1000) {
+            $errors[] = "Description cannot exceed 1000 characters.";
+        }
+
+        return $errors;
     }
 }
