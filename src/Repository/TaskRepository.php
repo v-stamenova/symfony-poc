05af2770e1dd872404df763c685dbafdfb2aa311
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-02-06 11:38:57 +0100

    Implement the doctrine connection in the repo

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -4,23 +4,25 @@ namespace App\Repository;
 
 use App\Entity\Task;
 use App\Repository\Interfaces\TaskRepositoryInterface;
+use Doctrine\DBAL\Connection;
 use PDO;
 
 class TaskRepository implements TaskRepositoryInterface
 {
-    private PDO $pdo;
+    private Connection $connection;
 
-    public function __construct()
+    public function __construct(Connection $connection)
     {
-        $this->pdo = new PDO('sqlite:' . __DIR__ . '/../../var/data/db.sqlite');
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connection = $connection;
     }
 
     public function findAll(): array
     {
-        $stmt = $this->pdo->query("SELECT * FROM tasks ORDER BY id DESC");
+        $sql = "SELECT * FROM tasks ORDER BY id DESC";
+        $tasksData = $this->connection->fetchAllAssociative($sql);
+
         $tasks = [];
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        foreach ($tasksData as $row) {
             $tasks[] = new Task($row['id'], $row['title'], $row['description']);
         }
         return $tasks;
@@ -28,28 +30,35 @@ class TaskRepository implements TaskRepositoryInterface
 
     public function find(int $id): ?Task
     {
-        $stmt = $this->pdo->prepare("SELECT * FROM tasks WHERE id = ?");
-        $stmt->execute([$id]);
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $sql = "SELECT * FROM tasks WHERE id = ?";
+        $row = $this->connection->fetchAssociative($sql, [$id]);
+
         return $row ? new Task($row['id'], $row['title'], $row['description']) : null;
     }
 
     public function insert(Task $task): bool
     {
-        $stmt = $this->pdo->prepare("INSERT INTO tasks (title, description) VALUES (?, ?)");
-        return $stmt->execute([$task->title, $task->description]);
+        $sql = "INSERT INTO tasks (title, description) VALUES (:title, :description)";
+        return $this->connection->executeStatement($sql, [
+                'title' => $task->title,
+                'description' => $task->description
+            ]) > 0;
     }
 
     public function update(int $id, Task $task): bool
     {
-        $stmt = $this->pdo->prepare("UPDATE tasks SET title = ?, description = ? WHERE id = ?");
-        return $stmt->execute([$task->title, $task->description, $id]);
+        $sql = "UPDATE tasks SET title = :title, description = :description WHERE id = :id";
+        return $this->connection->executeStatement($sql, [
+                'id' => $id,
+                'title' => $task->title,
+                'description' => $task->description
+            ]) > 0;
     }
 
     public function delete(int $id): bool
     {
-        $stmt = $this->pdo->prepare("DELETE FROM tasks WHERE id = ?");
-        return $stmt->execute([$id]);
+        $sql = "DELETE FROM tasks WHERE id = :id";
+        return $this->connection->executeStatement($sql, ['id' => $id]) > 0;
     }
 
 }
