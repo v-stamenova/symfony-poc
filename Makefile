7a853b100515c0fdadc1f47a4c07d209ae94f63e
Valeria Stamenova <77020646+v-stamenova@users.noreply.github.com>
2025-02-07 14:18:53 +0100

    Unit tests (#4)
    * Add phpunit
    
    * Add tests for the repository
    
    * Remove versioning from docker compose due to depr
    
    * Rename from `list` to `index`
    
    * Add tests for the controller
    
    * Make makefile for faster command execution
    
    * Install migrations package
    
    * Create migration for tasks
    
    * Add bash shortcut in the Makefile
    
    * Final fixes

---
+++ Makefile
@@ -0,0 +1,20 @@
+CONTAINER_NAME=symfony_app
+DB_FILE=$(shell php -r "echo str_replace('sqlite:///%kernel.project_dir%/', '$(shell pwd)/', getenv('DATABASE_URL')) ?: 'var/data/db.sqlite';")
+
+composer-install:
+	docker exec -it $(CONTAINER_NAME) composer require $(PACKAGE)
+
+phpunit:
+	docker exec -it $(CONTAINER_NAME) ./vendor/bin/phpunit
+
+bash:
+	docker exec -it $(CONTAINER_NAME) bash
+
+migrate:
+	docker exec -it $(CONTAINER_NAME) php bin/console doctrine:migrations:migrate --no-interaction
+
+migrate-fresh:
+	rm -f $(DB_FILE)
+	touch $(DB_FILE)
+	chmod 777 $(DB_FILE)
+	docker exec -it $(CONTAINER_NAME) php bin/console doctrine:migrations:migrate --no-interaction
---
+++ migrations/Version20250206122155.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250206122155 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Creates tasks table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                description TEXT
+            )
+        ");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("DROP TABLE IF EXISTS tasks");
+    }
+}
