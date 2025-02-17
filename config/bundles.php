d2fe18b052dd30b5bab956b09cdcaaa5bb269d4f
Valeria Stamenova <valeria.stamenova3@gmail.com>
2025-02-17 12:10:53 +0100

    Add tailwind

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
